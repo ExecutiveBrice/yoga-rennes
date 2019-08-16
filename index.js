1338be565a71c63bca63d8e60fd2e3b5c318fa57
brice <brice_morel@hotmail.com>
2019-08-17 00:31:16 +0200

    make it better

--- index.js
+++
@@ -1,8 +0,0 @@
-
-var express = require('express');
-var server = express();
-var options = {
-index: 'index.html'
-};
-server.use('/', express.static('/dist/yoga', options));
-server.listen(process.env.PORT);
